c3b409d11cfc7789acb944722389708da81987da
Brian Yeager <rbrianyeager@gmail.com>
2019-09-05 22:04:42 -0400

    the final, final submision

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -5,8 +5,7 @@ $( document ).ready(function() {
 var settings = {
     async: true,
     crossDomain: true,
-    url: "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3",
-    method: "GET",
+    url: "https://audd.p.rapidapi.com/"
     headers: {
         "x-rapidapi-host": "audd.p.rapidapi.com",
         "x-rapidapi-key": "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb",
@@ -17,7 +16,7 @@ $.ajax(settings).done(function (response) {
 });
 
 function searchBandsInTown(artist) {
-    var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
+    var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3  &apikey=af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
 $.ajax({
     url: queryURL,
     method: "GET"
