88b3fecd7daf2064993405778d9cf7ba6ba776a9
Brian Yeager <rbrianyeager@gmail.com>
2019-09-07 11:02:36 -0400

    removed unnecessary change and updated bio

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -5,7 +5,8 @@ $( document ).ready(function() {
 var settings = {
     async: true,
     crossDomain: true,
-    url: "https://audd.p.rapidapi.com/"
+    url: "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3",
+    method: "GET",
     headers: {
         "x-rapidapi-host": "audd.p.rapidapi.com",
         "x-rapidapi-key": "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb",
@@ -15,8 +16,8 @@ $.ajax(settings).done(function (response) {
     console.log(response);
 });
 
-function searchBandsInTown(artist) {
-    var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3  &apikey=af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
+function searchBands(artist) {
+    var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
 
 $.ajax({
     url: queryURL,
