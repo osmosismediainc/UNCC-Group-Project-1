3ee63ee4fcf563d810f8d5ddfe6b1e28acec26f7
Brian Yeager <rbrianyeager@gmail.com>
2019-09-08 18:35:19 -0400

    Added IDs to musicapp.js file for dumping the results onto the page

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -13,27 +13,27 @@ $( document ).ready(function () {
         }
     }
 
-    //$.ajax(settings).done(function (response) {
-    //console.log(response);//
+    $.ajax(settings).done(function (response) {
+    console.log(response);//
 });
 
 function searchBands(artist) {
-    //var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
+    var queryURL = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb";
 
-    //$.ajax({
-    //url: queryURL,//
-    // method: "GET"
-    // }).then(function(response) {
+    $.ajax({
+    url: queryURL,//
+    method: "GET"
+    }).then(function(response) {
 
-    // Printing the entire object to console
+    Printing the entire object to console
     console.log(response);
 
     // Constructing HTML containing the artist information
-    var artistName = $("<h1>").text(response.name);
-    var artistLyrics = $("<h1>").text(response.lyrics);
-    var artistTitle = $("<img>").attr("src", response.title);
-    var artistAlbum = $("<img>").attr("src", response.album);
-    var artistArtwork = $("<img>").attr("src", response.artwork);
+    var artistName = $("#artist").text(response.name);
+    var artistLyrics = $("#lyrics").text(response.lyrics);
+    var artistTitle = $("#song").attr("src", response.title);
+    var artistAlbum = $("#album").attr("src", response.album);
+    var artistArtwork = $("#albumArt").attr("src", response.artwork);
 
 
     // Empty the contents of the artist-div, append the new artist content
