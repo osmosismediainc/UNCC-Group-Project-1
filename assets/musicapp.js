7e54bbba53459bfb6f8100eea721198dc1b4735a
Brian Yeager <rbrianyeager@gmail.com>
2019-09-10 17:45:26 -0400

    audd api pulling results from lyrics input and can handle multiple inputs consecutively

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -28,6 +28,12 @@ $(document).ready(function () {
     $.ajax(settings).done(function (response) {
       console.log(response);
 
+      var results = response.data;
+
+      // Empty the contents of the artist-div, append the new artist content
+      $("#musicInfoHolder").empty();
+      $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
+
       // Constructing HTML containing the artist information
       var artistName = $("#artist").text(response.name);
       var artistLyrics = $("#lyrics").text(response.lyrics);
@@ -36,9 +42,7 @@ $(document).ready(function () {
       var artistArtwork = $("#albumArt").text(response.artwork);
 
 
-      // Empty the contents of the artist-div, append the new artist content
-      $("#searchResultsDiv").empty();
-      $("#searchResultsDiv").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
+      
     });
 
 
@@ -54,55 +58,6 @@ $(document).ready(function () {
 
 
 
-
-    //   // Running the searchBandsInTown function(passing in the artist as an argument)
-    //   searchBands(inputArtist);
-
-    //       // Preventing the button from trying to submit the form
-    //       // Storing the artist name
-    //       var inputArtist = $("#musicInfoHolder").val().trim();
-    //       function searchMusic(artist) {
-    //         var queryURL = "https://cors-anywhere.herokuapp.com/https://api.audd.io/findLyrics/?q=say my name" + "&apikey=eb0db3e732msh30354716e533ef7p18e1a7jsnfa3cae64ef01";
-
-    //         $.ajax({
-    //             url: queryURL,
-    //             method: "POST"
-    //         }).then(function (response) {
-
-    //             // Printing the entire object to console
-    //             console.log(response);
-
-    //             // Constructing HTML containing the artist information
-    //             var artistName = $("<h1>").text(response.name);
-    //             artistLyrics = $("<p>").text(response.lyrics);
-    //             var artistTitle = $("<p>").attr("src", response.title);
-    //             var artistAlbum = $("<h1>").attr("src", response.album);
-    //             var artistArtwork = $("<img>").attr("src", response.artwork);
-
-
-    //             // Empty the contents of the artist-div, append the new artist content
-    //             $("#musicInfoHolder").empty();
-    //             $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
-    //         });
-
-    //     };
-    //       // Running the searchBandsInTown function(passing in the artist as an argument)
-    //       searchMusic(inputArtist);
-
-    //       var form = document.getElementById('musicFileInput');
-    //       form.onsubmit = function () {
-    //           var formData = new FormData(form);
-
-    //           formData.append('file', file);
-
-    //           var xhr = new XMLHttpRequest();
-    //           // Add any event handlers here...
-    //           xhr.open('POST', form.getAttribute('action'), true);
-    //           xhr.send(formData);
-
-    //           return false; // To avoid actual submission of the form
-    //       }
-    // })
 
   });
 });
