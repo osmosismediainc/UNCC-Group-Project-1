c1648abb658621c3629445049d3cbfc53c04208d
Brian Yeager <rbrianyeager@gmail.com>
2019-09-10 19:15:07 -0400

    need to render the results to the page

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -28,18 +28,24 @@ $(document).ready(function () {
     $.ajax(settings).done(function (response) {
       console.log(response);
 
-      var results = response.data;
+      var result = response.data;
+     
+      var a = $("<p>").text(result[i].artist)
+      
+
+
+     // Constructing HTML containing the artist information
+     var artistName = $("#artist").text(response.artist);
+     var artistLyrics = $("#lyrics").text(response.lyrics);
+     var artistTitle = $("#song").text(response.title);
+     
+
 
       // Empty the contents of the artist-div, append the new artist content
       $("#musicInfoHolder").empty();
-      $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
+      $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName);
 
-      // Constructing HTML containing the artist information
-      var artistName = $("#artist").text(response.name);
-      var artistLyrics = $("#lyrics").text(response.lyrics);
-      var artistTitle = $("#song").text(response.title);
-      var artistAlbum = $("#album").text(response.album);
-      var artistArtwork = $("#albumArt").text(response.artwork);
+      
 
 
       
