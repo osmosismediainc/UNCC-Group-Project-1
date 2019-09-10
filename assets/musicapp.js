53f7240e99e8df219a61e8effa66288c8ab96938
Brian Yeager <rbrianyeager@gmail.com>
2019-09-09 23:31:21 -0400

    musicapp.js redo sort of

--- assets/musicapp.js
+++ assets/musicapp.js
@@ -1,104 +1,149 @@
-$( document ).ready(function () {
-  console.log( "yooooo" );
+$(document).ready(function () {
+  console.log("yooooo");
+  // Define some variables
+  var lyrics = "";
+  var apiKey = "af514938d5msha11b1d9e2c8a7fcp14c174jsn02b30455c6eb"
 
-
-
-function searchBands(artist) {
-  var queryURL = "https://cors-anywhere.herokuapp.com/https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "&apikey=eb0db3e732msh30354716e533ef7p18e1a7jsnfa3cae64ef01";
-
-  $.ajax({
-  url: queryURL,
-  method: "GET"
-  }).then(function(response) {
-
-  //Printing the entire object to console
-  console.log(response);
-
-  // Constructing HTML containing the artist information
-  var artistName = $("#artist").text(response.name);
-  var artistLyrics = $("#lyrics").text(response.lyrics);
-  var artistTitle = $("#song").attr("src", response.title);
-  var artistAlbum = $("#album").attr("src", response.album);
-  var artistArtwork = $("#albumArt").attr("src", response.artwork);
-
-
-  // Empty the contents of the artist-div, append the new artist content
-  $("#searchResultsDiv").empty();
-  $("#searchResultsDiv").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
-});
-
-};
-$("#musicFileSubmit").on("click", function (event) {
-  // Preventing the button from trying to submit the form
-  event.preventDefault();
-  // Storing the artist name
-  var inputArtist = $("#musicResults").val().trim();
-
-  // Running the searchBandsInTown function(passing in the artist as an argument)
-  searchBands(inputArtist);
-
-      // Preventing the button from trying to submit the form
-      // Storing the artist name
-      var inputArtist = $("#musicInfoHolder").val().trim();
-      function searchMusic(artist) {
-        var queryURL = "https://cors-anywhere.herokuapp.com/https://api.audd.io/findLyrics/?q=say my name" + "&apikey=eb0db3e732msh30354716e533ef7p18e1a7jsnfa3cae64ef01";
-
-        $.ajax({
-            url: queryURL,
-            method: "POST"
-        }).then(function (response) {
-
-            // Printing the entire object to console
-            console.log(response);
-
-            // Constructing HTML containing the artist information
-            var artistName = $("<h1>").text(response.name);
-            artistLyrics = $("<p>").text(response.lyrics);
-            var artistTitle = $("<p>").attr("src", response.title);
-            var artistAlbum = $("<h1>").attr("src", response.album);
-            var artistArtwork = $("<img>").attr("src", response.artwork);
-
-
-            // Empty the contents of the artist-div, append the new artist content
-            $("#musicInfoHolder").empty();
-            $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
-        });
-
-    };
-      // Running the searchBandsInTown function(passing in the artist as an argument)
-      searchMusic(inputArtist);
-
-      var form = document.getElementById('musicFileInput');
-      form.onsubmit = function () {
-          var formData = new FormData(form);
-
-          formData.append('file', file);
-
-          var xhr = new XMLHttpRequest();
-          // Add any event handlers here...
-          xhr.open('POST', form.getAttribute('action'), true);
-          xhr.send(formData);
-
-          return false; // To avoid actual submission of the form
+    // function to add the user's lyric input to the lyrics variable
+    $("#lyricSearchSubmit").on("click", function (event) {
+      event.preventDefault();
+      var lyricsInput = $("#lyricSearch").val().trim();
+      if (!lyricsInput == " ") {
+        lyrics.push(lyricsInput);
+        console.log(lyrics);
       }
-})
-// var x = document.getElementById("myFile");
-// var txt = "";
-// if ('files' in x) {
-//   if (x.files.length == 0) {
-//     txt = "Select one or more files.";
-//   } else {
-//     for (var i = 0; i < x.files.length; i++) {
-//       txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-//       var file = x.files[i];
-//       if ('name' in file) {
-//         txt += "name: " + file.name + "<br>";
-//       }
-//       if ('size' in file) {
-//         txt += "size: " + file.size + " bytes <br>";
-//       }
-//     }
-//   }
-// }
-// document.getElementById ("demo").innerHTML = txt;
-})
+
+
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://audd.p.rapidapi.com/findLyrics/?q=" + lyrics + apiKey,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "audd.p.rapidapi.com",
+      "x-rapidapi-key": "5da9678411msh0bf02b451454513p111fc5jsn1344d0dc18f3"
+    }
+  }
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+
+    // var lyrics = $(this).attr("data-lyrics");
+    // var queryURL = "https://cors-anywhere.herokuapp.com/https://api.audd.io/findLyrics/?q=" + lyrics + apiKey;
+
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // }).then(function(response) {
+    //   console.log(queryURL);
+    //   console.log(response);
+
+    //   var results = response.data;
+  })
+// });
+
+
+
+
+
+  //   function searchBands(artist) {
+  //   var queryURL = "https://cors-anywhere.herokuapp.com/https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3" + artist + "&apikey=eb0db3e732msh30354716e533ef7p18e1a7jsnfa3cae64ef01";
+
+  //   $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  //   }).then(function(response) {
+
+  //   //Printing the entire object to console
+  //   console.log(response);
+
+  //   // Constructing HTML containing the artist information
+  //   var artistName = $("#artist").text(response.name);
+  //   var artistLyrics = $("#lyrics").text(response.lyrics);
+  //   var artistTitle = $("#song").attr("src", response.title);
+  //   var artistAlbum = $("#album").attr("src", response.album);
+  //   var artistArtwork = $("#albumArt").attr("src", response.artwork);
+
+
+  //   // Empty the contents of the artist-div, append the new artist content
+  //   $("#searchResultsDiv").empty();
+  //   $("#searchResultsDiv").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
+  // });
+
+  // };
+  // $("#musicFileSubmit").on("click", function (event) {
+  //   // Preventing the button from trying to submit the form
+  //   event.preventDefault();
+  //   // Storing the artist name
+  //   var inputArtist = $("#musicResults").val().trim();
+
+  //   // Running the searchBandsInTown function(passing in the artist as an argument)
+  //   searchBands(inputArtist);
+
+  //       // Preventing the button from trying to submit the form
+  //       // Storing the artist name
+  //       var inputArtist = $("#musicInfoHolder").val().trim();
+  //       function searchMusic(artist) {
+  //         var queryURL = "https://cors-anywhere.herokuapp.com/https://api.audd.io/findLyrics/?q=say my name" + "&apikey=eb0db3e732msh30354716e533ef7p18e1a7jsnfa3cae64ef01";
+
+  //         $.ajax({
+  //             url: queryURL,
+  //             method: "POST"
+  //         }).then(function (response) {
+
+  //             // Printing the entire object to console
+  //             console.log(response);
+
+  //             // Constructing HTML containing the artist information
+  //             var artistName = $("<h1>").text(response.name);
+  //             artistLyrics = $("<p>").text(response.lyrics);
+  //             var artistTitle = $("<p>").attr("src", response.title);
+  //             var artistAlbum = $("<h1>").attr("src", response.album);
+  //             var artistArtwork = $("<img>").attr("src", response.artwork);
+
+
+  //             // Empty the contents of the artist-div, append the new artist content
+  //             $("#musicInfoHolder").empty();
+  //             $("#musicInfoHolder").append(artistLyrics, artistTitle, artistName, artistAlbum, artistArtwork);
+  //         });
+
+  //     };
+  //       // Running the searchBandsInTown function(passing in the artist as an argument)
+  //       searchMusic(inputArtist);
+
+  //       var form = document.getElementById('musicFileInput');
+  //       form.onsubmit = function () {
+  //           var formData = new FormData(form);
+
+  //           formData.append('file', file);
+
+  //           var xhr = new XMLHttpRequest();
+  //           // Add any event handlers here...
+  //           xhr.open('POST', form.getAttribute('action'), true);
+  //           xhr.send(formData);
+
+  //           return false; // To avoid actual submission of the form
+  //       }
+  // })
+  // // var x = document.getElementById("myFile");
+  // // var txt = "";
+  // // if ('files' in x) {
+  // //   if (x.files.length == 0) {
+  // //     txt = "Select one or more files.";
+  // //   } else {
+  // //     for (var i = 0; i < x.files.length; i++) {
+  // //       txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+  // //       var file = x.files[i];
+  // //       if ('name' in file) {
+  // //         txt += "name: " + file.name + "<br>";
+  // //       }
+  // //       if ('size' in file) {
+  // //         txt += "size: " + file.size + " bytes <br>";
+  // //       }
+  // //     }
+  // //   }
+  // // }
+  // // document.getElementById ("demo").innerHTML = txt;
+}); 
